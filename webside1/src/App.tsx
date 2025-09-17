79da5f065f885f82df2679dc0a4fbaf5049bb66d
ChristinaLim888 <Christinalim666@gmail.com>
2025-09-17 17:39:54 +0700

    tes

---
+++ webside1/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind + React + TS 🎉
+      </h1>
+      <p className="mt-4 text-gray-600">
+        Webside ini akan pakai komponen-komponen
+      </p>
+    </div>
+  )
+}
+
+
+export default App
